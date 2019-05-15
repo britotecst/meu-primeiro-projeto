8deaa428e936c9e9e6416b96f455ff3477488bcb
Renato Santana Brito <britotecst@hotmail.com>
2019-05-14 23:13:40 -0300

    update

--- src/main/java/com/kazale/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/kazale/api/MeuPrimeiroProjetoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MeuPrimeiroProjetoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
-        System.out.println("Meu primeiro projeto");
+        System.out.println("Meu primeiro projeto agora com Travis");
 
     }
 }
